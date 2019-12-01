355383be21367d4f7f528a358839b8d3104b1a6c
dnabre <alexander@happybunnylabs.com>
2019-12-01 06:01:31 -0700

    initial aoc_1.py

---
+++ aoc_1.py
@@ -0,0 +1,13 @@
+
+filename = "aoc_1_input.txt"
+
+def main():
+  f = open(filename, "r")
+  lines = f.readlines()
+  for l in lines:
+    print(l)
+    
+    
+    
+if __name__ = "__main__":
+  main()  
