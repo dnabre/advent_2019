0d9e91d23a020747ab635c39cad7b797ae613c73
dnabre <dnabre@gmail.com>
2019-12-04 16:33:22 +0000

    Part 4.1 complete and correct

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-import aoc_3
+import aoc_4
+""" This module/file is just to direct execution to the current problem being worked on
+	This is only because of the (apparent) limitations of http://repl.it
+"""
 
-aoc_3.main()
+
+
+aoc_4.main()
