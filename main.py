d357b63a378a02e53fb38c47bee5fa247d9bed5b
dnabre <alexander@happybunnylabs.com>
2019-12-09 22:02:14 -0700

    AoC 8 Part #1 Complete and Correct

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import aoc_7
+import aoc_8
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
 
 
 
-aoc_7.main()
+aoc_8.main()
