ec493c4fcf1756c435904054af96f6c4d54e6d36
dnabre <alexander@happybunnylabs.com>
2019-12-12 06:24:06 -0700

    IntCodeMachine progress
    RELATIVE_MODE added
    adj_relative instruction added
    auto-expanding added (via custom list class with getter/setter)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import aoc_8
+import aoc_9
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
 
 
 
-aoc_8.main()
+aoc_9.main()
