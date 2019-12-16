fe13a898116c3ae47dc2099ed786f2e82e1a202f
dnabre <alexander@happybunnylabs.com>
2019-12-16 13:48:37 -0700

    Part 1 of Day 16 working
    Needs a completely different approach for second part (generic solution blows up really big)

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import aoc_12
+import aoc_16
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
 
 
 
-aoc_12.main()
+aoc_16.main()
