ea5f0824835b93b60c84bf05362075f49f32e6fb
dnabre <dnabre@gmail.com>
2019-12-20 16:46:38 +0000

    cleaning up aoc12

--- aoc_12.py
+++ aoc_12.py
@@ -1,5 +1,6 @@
 import re
-import aocd
+import numpy as np
+#import aocd
 
 scan1 = [
 	'<x=-1, y=0, z=2>',
@@ -9,102 +10,127 @@ scan1 = [
 ]
 
 
+def vector3d(x,y,z):
+	return np.array([x,y,z])
+
 def parse_scan(scan1):
 	result = []
+	moons = []
 	for line in scan1:
 		line = line[1:-1]
 		p = re.findall(r'-?\d+', line)  # this is a handy little RE found in a tutorial
 		vals = [int(v) for v in p]
 		result.append(vals)
-	return result
+		moons.append(vector3d(vals[0],vals[1],vals[2]))
+	return moons
+
+
+
+class Moon:
+	'''
+	init_pos - save where we started
+	current_pos
+	velocity
+	accel
+	net_force - sum of forces being applied in current time step
+	
+	
+	'''
+	def __init__(self,initial_pos):
+		self.init_pos = initial_pos.copy()
+		self.current_pos = initial_pos.copy()
+		self.net_force = np.array([0,0,0])
+		self.accel = np.array([0,0,0,])
+		self.velocity = np.array([0,0,0,])
+	#print(" {:3d} ".format(x), end='')
+	
+	@staticmethod 
+	def vec2str(vec):
+		s = '{:5d},{:5d},{:5d}'.format(vec[0],vec[1],vec[2])
+		return s
+
+	def __str__(self):
+		result = ''
+		result += 'Moon @ <{:s}> with vel: <{:s}>, accel: <{:s}>, net_f: <{:s}>'.format(
+			Moon.vec2str(self.current_pos),
+			Moon.vec2str(self.velocity),
+			Moon.vec2str(self.accel),
+			Moon.vec2str(self.net_force)
+		)
+		return result
+	def apply_gravity(self,other_moon):
+
+
+		return None
+
+	def apply_forces(self):
+		
+		self.net_force=np.array([0,0,0])
+		return None
+
+	def step(self):
+
+		self.accel = np.array([0,0,0])
+		return None
+
+
+
 
 
 def print_world(moons, n):
 	print(f'After {n} steps:')
-	for (pos, vel) in moons:
-		(x, y, z) = pos
-		print('pos=<x={:3d}, y={:3d}, z={:3d}>, vel=<'.format(x, y, z), end='')
-		(x, y, z) = vel
-		print('x={:3d}, y={:3d}, z={:3d}>\n'.format(x, y, z), end='')
+	for m in moons:
+		p = Moon.vec2str( m.current_pos)
+		v = Moon.vec2str( m.velocity)
+		print('pos=<{:s}>, vel=<{:s}>'.format(p, v), end='')
+		print()
 	print()
 
 
-def calc_gravity(a, b):
-	(p1, v1) = a
-	(p2, v2) = b
-	(x1, y1, z1) = (p1[0], p1[1], p1[2])
-	(x2, y2, z2) = (p2[0], p2[1], p2[2])
-
-	(v_x1, v_y1, v_z1) = (v1[0], v1[1], v1[2])
-	(v_x2, v_y2, v_z2) = (v2[0], v2[1], v2[2])
-
-	# x-axiz
-	if (x1 < x2):
-		v_x1 += 1
-		v_x2 += -1
-	else:
-		v_x1 += -1
-		v_x2 += 1
-
-	if (y1 < y2):
-		v_y1 += 1
-		v_y2 += -1
-	else:
-		v_y1 += -1
-		v_y2 += 1
-
-	if (z1 < z2):
-		v_z1 += 1
-		v_z2 += -1
-	else:
-		v_z1 += -1
-		v_z2 += 1
-	return ((v_x1, v_y1, v_z1), (v_x2, v_y2, v_z2))
 
 
-def add_vel(p, v):
-	(x, y, z) = p
-	(a, b, c) = v
-	return (x + a, y + b, c + z)
 
 
-# TODO: scrap and do over
-def next_time_step(moons):
-	for a in range(0, len(moons)):
-		for b in range(a + 1, (len(moons))):
-			x, y = calc_gravity(moons[a], moons[b])
-			moons[a] = x
-			moons[b] = y
 
-	for i in range(len(moons)):
-		(p, v) = moons[i]
-		n_p = add_vel(p, v)
-		moons[i] = (n_p, v)
 
-	return moons
+puzzle = [
+	'<x=-6, y=-5, z=-8>',
+	'<x=0, y=-3, z=-13>',
+	'<x=-15, y=10, z=-11>',
+	'<x=-3, y=-8, z=3>']
 
 
 def main():
-	puzzle = aocd.models.Puzzle(year=2019, day=12)
-	print(puzzle)
-	print(puzzle.input_data)
-# print(scan1)
-#	print()
-'''
-	pos =  parse_scan(scan1)
-	vels = []
-	for _ in pos:
-		vels.append([0,0,0])
-	moons =[m for m in zip(pos,vels)]
+	pos = parse_scan(scan1)
+	print(pos)
+	world_time_step = 0
 
-	n_max = 10
-	print_world(moons,0)
+	#setup and display initial moon list
+	moon_list = []
+	for m in pos:
+		n_m = Moon(m)
+		moon_list.append(n_m)
+	print_world(moon_list, world_time_step)
 
-	for x in range(1,n_max+1):
-		moons = next_time_step(moons)
-		print_world(moons, x)
-'''
+	#Have each moon receive the force of gravity from all moons (excluding itseslf of course)
+	for m in moon_list:
+		for other_m in moon_list:
+			if(m==other_m): continue
+			else:
+				m.apply_gravity(other_m)
 
+	#Have each moon tally all the forces being applied to it, and calculate it's new acceleration
+	# NOTE: this will zero all forces which have been applied
+	for m in moon_list:
+		m.apply_forces()
+
+	#Have each moon progress time one step. So it's acceleration is applied to its velocity and the moon's position to updated
+	# NOTE: this will zero acceleration after it has  have been applied
+	for m in moon_list:
+		m.step()
+	world_time_step += 1
+
+	print_world(moon_list, world_time_step)
 
 if __name__ == "__main__":
 	main()
