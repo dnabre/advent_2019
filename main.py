56d687b3970127184d23c8caa375f2e6d4bf28e8
dnabre <alexander@happybunnylabs.com>
2019-12-22 03:41:45 -0700

    working problem #22p1
    debugging

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import aoc_14
+import aoc_22
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
 
 
 
-aoc_14.main()
+aoc_22.main()
