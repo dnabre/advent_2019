3384352f9e96b4470ec7fab58a98ede50b5cfd97
dnabre <dnabre@gmail.com>
2020-01-01 16:02:42 +0000

    aoc_14p1 intial working solution, need to test more

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import aoc_24
+import aoc_14
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
 
 
 
-aoc_24.main()
+aoc_14.main()
