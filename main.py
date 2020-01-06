75f803db26ab3a3208a84d546a5130005650a88a
dnabre <dnabre@gmail.com>
2020-01-06 14:14:58 +0000

    aoc_18
    random map functions

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import aoc_14
+import aoc_18
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
-
-
-
-aoc_14.main()
+aoc_18.main()
