c1549011cff83658a70ce0068625bc3c0ea2a95e
dnabre <alexander@happybunnylabs.com>
2020-01-08 15:06:29 -0700

    day 20 part1
    parsing with portals done

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import aoc_18
+import aoc_20
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
-aoc_18.main()
+aoc_20.main()
