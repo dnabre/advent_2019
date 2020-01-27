c53f9fa2ed125260dd3c9060de1373ce485599d4
dnabre <alexander@happybunnylabs.com>
2020-01-27 01:43:00 -0700

    aoc_20 (yak shaving)
    basic graph
    plot to file

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import aoc_20
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
-aoc_20.main(2)
+aoc_20.main()
