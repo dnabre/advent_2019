ccfaed746b5bc9c5c4b988f09bd7cb654cb453ae
dnabre <alexander@happybunnylabs.com>
2020-11-18 16:08:03 -0700

    Adding note about missing code

---
+++ aoc_20.py
@@ -0,0 +1,26 @@
+
+
+
+
+# Lost the code for this problem somewhere. Downside of jumping back 
+# and forth between machines and IDEs. Likely could have been avoided
+# if I was using git properly.
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+# Part 1 Answer:  604
+# Part 2 Answer: 7166
