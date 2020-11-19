f50e03bdf6ec18ccb23badb456e5716b155f747c
dnabre <alexander@happybunnylabs.com>
2020-11-18 17:42:08 -0700

    README.md: add aoc_20 finished mark
    aoc_18 basic parsing, and data for BFS search setup
    
    will have small test cases aoc_18_test_{1,2,3...}.txt

--- aoc_18.py
+++ aoc_18.py
@@ -1,135 +1,113 @@
 
-import math
+import os
+import collections
 
-example_1 = """\
-#########
-#b.A.@.a#
-#########
-"""
+import string
 
-example_2 = """\
-########################
-#f.D.E.e.C.b.A.@.a.B.c.#
-######################.#
-#d.....................#
-########################
-"""
-
-
-example_3 = """\
-########################
-#...............b.C.D.f#
-#.######################
-#.....@.a.B.c.d.A.e.F.g#
-########################
-"""
+def key_format(k):
+	if(k==''):
+		return '-'*len(keys)
+	r = ''
+	for i in keys:
+		if(i in k):
+			r = r + i
+		else:
+			r = r + '-'
+	return r
 
 
-example_4 = """\
-#################
-#i.G..c...e..H.p#
-########.########
-#j.A..b...f..D.o#
-########@########
-#k.E..a...g..B.n#
-########.########
-#l.F..d...h..C.m#
-#################
-"""
+class Location:
+	def __init__(self,x,y,keys=''):
+		self.x = x
+		self.y = y
+		self.keys = keys
+
+	def __str__(self):
+		return '[{},{},{}]'.format(self.x,self.y,key_format(self.keys))
+
+	__repr__ = __str__
 
 
-example_5 = """\
-########################
-#@..............ac.GI.b#
-###d#e#f################
-###A#B#C################
-###g#h#i################
-########################
-"""
-
-example_steps = [8,86, 132,136,81]
-examples = [example_1,example_2,example_3,example_4,example_5]
-target = example_4
-AOC_18_DATA_FILENAME='aoc_18_input.txt'
+start_loc = None
+keys=''
+doors=''
 
 
-def part_1_map():
-	with open(AOC_18_DATA_FILENAME, 'r') as input_file:
-		prob_map = input_file.readlines()
-	return prob_map
+places = set()
+walls = set()
+key_locations = set()
+door_locations = set()
 
-def get_dims(map):
-	x = len(map)
-	y = len(map[0])
-	return (y,x)
+CURRENT_FILE = 'aoc_18_test_1.txt'
 
-def map2array(map):
-	result = []
-	rows = map.split('\n')
-	for r in rows: r = r.strip()
-	rows = rows[:-1]
-	for r in rows:
-		new_row=[]
-		for c in r:
-			new_row.append(c)
-#			print(c,end='')
-#		print()
-		result.append(new_row)
-	return result
+def process_map_by_character(inp):
+	global keys
+	global doors
+	global key_locations
+	global door_locations
+	global places
+	global start_loc
 
-def make_paded_array(map):
-	n_rows = len(map)
-	n_cols = len(map[0])
-	print((n_rows,n_cols))
-	for r in map:
-		r.append('#')
-		r.insert(0,'#')	
-	row_length = len(map[0])
-	map.insert(0,(['#'] * row_length))
-	map.append(['#'] * row_length)
-	return map
+	x=0
+	y=0
 
-def print_map(map):
-	for r in map:
-		for c in r:
-			print(c,end='')
-		print()
-def coords_for(map, ch):
-	x = 0
-	y = 0
-	for (y,r) in enumerate(map):
-		
-		for (x,c) in enumerate(r):
-			if(c == ch):
-				return (x,y)
-			
-		
-	return (-1,-1)
-def map2dict(map):
-	map = make_paded_array(map)
-	m = dict()
-	for (y,r) in enumerate(map):
-		for (x,c) in enumerate(r):
-			m[(x-1,y-1)] = c
-	return m
+	for c in inp:
+		if(c=='@'):
+			start_loc= Location(x,y)
+		elif(c=='\n'):
+			x=0
+			y = y + 1
+		elif(c in string.ascii_lowercase):
+			keys = keys + c
+			key_locations.add(Location(x,y))
+			places.add(Location(x, y))
+		elif(c in string.ascii_uppercase):
+			doors = doors + c
+			door_locations.add(Location(x,y))
+			places.add(Location(x,y))
+		elif( c == '.'):
+			places.add(Location(x,y))
+		else:
+			if(c!='#'):
+				print('Error got {} in input stream. Expected #.'.format(c))
+			walls.add(Location(x,y))
+		x=x+1
 
-markers = '@,a,b,c,d,e,f,g,H,A,B,C,D,E,F,G,H'
+		keys = ''.join(sorted(keys))
+		doors = ''.join(sorted(keys))
+
+
+
+
+
+
+
+
+
+
 def main():
-	#print(target)
-	map = map2array(target)
-	print_map(map)	
-	#print(get_dims(map))
-	dims = get_dims(map)
-	print(dims)
-	pd_map = map2dict(map)
-	
-	for y in range(dims[1]):
-		for x in range(dims[0]):
-			c = pd_map[(x,y)]
-			print(c,end='')
-		print()
+	print('AOC_2019\t 18\n\n')
 
-	
+
+
+	with open(CURRENT_FILE, 'r') as input_file:
+		all_file = input_file.read()
+	print(all_file)
+
+	process_map_by_character(all_file)
+
+	print('start_loc:\t {}'.format(start_loc))
+	print('keys:\t {}'.format(keys))
+	print('doors:\t {}'.format(doors))
+
+	print('key_locations:\t {}'.format(key_locations))
+	print('door_locations:\t {}'.format(door_locations))
+	print('places: \n {}'.format(places))
+	print('walls: \n {}'.format(walls))
+
+
+
+
 
 if __name__ == "__main__":
-	main()
+    main()
