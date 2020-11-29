030ed7db5d4e9eea90a39af5df561407063cb6a4
repl.it user <replituser@example.com>
2020-11-29 15:00:36 +0000

    added example problems to aoc_18

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import aoc_20
+import aoc_18
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
-aoc_20.main()
+aoc_18.main()
