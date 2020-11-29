c09be4a1d58dd7da947d9fbe8aab939c8d7ff8fd
Matthew Barrick <dnabre@gmail.com>
2020-11-29 15:54:46 +0000

    redid parser for aoc_18
    map and points of interest all parsed out

--- aoc_18.py
+++ aoc_18.py
@@ -4,6 +4,52 @@ import collections
 
 import string
 
+
+example_1 = """\
+#########
+#b.A.@.a#
+#########"""
+
+example_2 = """\
+########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################"""
+
+
+example_3 = """\
+########################
+#...............b.C.D.f#
+#.######################
+#.....@.a.B.c.d.A.e.F.g#
+########################"""
+
+
+example_4 = """\
+#################
+#i.G..c...e..H.p#
+########.########
+#j.A..b...f..D.o#
+########@########
+#k.E..a...g..B.n#
+########.########
+#l.F..d...h..C.m#
+#################"""
+
+
+example_5 = """\
+########################
+#@..............ac.GI.b#
+###d#e#f################
+###A#B#C################
+###g#h#i################
+########################"""
+
+example_steps = [8,86, 132,136,81]
+
+currect_map = example_1
+
 def key_format(k):
 	if(k==''):
 		return '-'*len(keys)
@@ -14,6 +60,14 @@ def key_format(k):
 		else:
 			r = r + '-'
 	return r
+
+def print_map(m):
+	for y in range(0,len(m[0])):
+		for x in range(0,len(m)):
+			print(m[x][y],end='')
+		print()
+
+
 
 
 class Location:
@@ -28,84 +82,65 @@ class Location:
 	__repr__ = __str__
 
 
-start_loc = None
-keys=''
-doors=''
 
-
-places = set()
-walls = set()
-key_locations = set()
-door_locations = set()
 
 CURRENT_FILE = 'aoc_18_test_1.txt'
 
-def process_map_by_character(inp):
-	global keys
-	global doors
-	global key_locations
-	global door_locations
-	global places
-	global start_loc
-
-	x=0
-	y=0
-
-	for c in inp:
-		if(c=='@'):
-			start_loc= Location(x,y)
-		elif(c=='\n'):
-			x=0
-			y = y + 1
-		elif(c in string.ascii_lowercase):
-			keys = keys + c
-			key_locations.add(Location(x,y))
-			places.add(Location(x, y))
-		elif(c in string.ascii_uppercase):
-			doors = doors + c
-			door_locations.add(Location(x,y))
-			places.add(Location(x,y))
-		elif( c == '.'):
-			places.add(Location(x,y))
-		else:
-			if(c!='#'):
-				print('Error got {} in input stream. Expected #.'.format(c))
-			walls.add(Location(x,y))
-		x=x+1
-
-		keys = ''.join(sorted(keys))
-		doors = ''.join(sorted(keys))
 
 
-
-
-
-
-
+start_loc = None
+keys=dict()
+doors=dict()
 
 
 
 def main():
 	print('AOC_2019\t 18\n\n')
+	global start_loc
+	global keys
+	global doors
+	
+	lines = currect_map.split('\n')
+	height = len(lines)
+	width = len(lines[0])
+
+	print((width,height))
+	map=[]	
+	for l in lines:
+		map.append(list(l))
+	
+	map = [*zip(*map)]
+	r=[]
+	for t in map:
+		r.append(list(t))
+	
+	map=r
+
+	#for y in range(0,height):
+	#	for x in range(0,width):
+	#		print(map[x][y],end='')
+	#	print()
+
+	print_map(map)
+
+	for y in range(0,height):
+		for x in range(0,width):
+			ch = map[x][y]
+			if(ch == '@'):
+				start_loc=(x,y)
+				map[x][y]='.'			
+			if(ch in string.ascii_lowercase):
+				keys[(x,y)] = ch
+				map[x][y]='.'
+			if(ch in string.ascii_uppercase):
+				doors[(x,y,)] = ch
+				map[x][y]='.'
 
 
-
-	with open(CURRENT_FILE, 'r') as input_file:
-		all_file = input_file.read()
-	print(all_file)
-
-	process_map_by_character(all_file)
-
-	print('start_loc:\t {}'.format(start_loc))
-	print('keys:\t {}'.format(keys))
-	print('doors:\t {}'.format(doors))
-
-	print('key_locations:\t {}'.format(key_locations))
-	print('door_locations:\t {}'.format(door_locations))
-	print('places: \n {}'.format(places))
-	print('walls: \n {}'.format(walls))
-
-
+	print_map(map)
+	print('start: {}'.format(start_loc))
+	print('keys : {}'.format(keys))
+	print('doors: {}'.format(doors))
 
 
 
