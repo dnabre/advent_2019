d42862914a0e395f8424f65141f7dccc0fe97a9c
Matthew Barrick <dnabre@gmail.com>
2020-12-02 16:52:35 +0000

    still working on bfs
    hating python more daily

--- aoc_18.py
+++ aoc_18.py
@@ -48,8 +48,7 @@ example_5 = """\
 
 example_steps = [8,86, 132,136,81]
 
-currect_map = example_1
-
+'''
 def key_format(k):
 	if(k==''):
 		return '-'*len(keys)
@@ -60,6 +59,9 @@ def key_format(k):
 		else:
 			r = r + '-'
 	return r
+'''
+def key_format(k):	
+	return "".join(k)
 
 def print_map(m):
 	for y in range(0,len(m[0])):
@@ -71,13 +73,28 @@ def print_map(m):
 
 
 class Location:
-	def __init__(self,x,y,keys=''):
+	def __init__(self,x,y,steps=0,keys=False):
 		self.x = x
 		self.y = y
-		self.keys = keys
+		if(keys==False):
+			self.keys = list(empty_key_string)
+		else:
+			self.keys = keys
+		self.steps = steps
+
+	def addKey(self,new_key):
+		n = loweralpha_to_num[new_key]
+		self.keys[n] = new_key
+
+	def isGoal(self):
+		if('-' in self.keys):
+			return False
+		else:
+			return True
+
 
 	def __str__(self):
-		return '[{},{},{}]'.format(self.x,self.y,key_format(self.keys))
+		return '[{},{},{},{}]'.format(self.x,self.y,key_format(self.keys),self.steps)
 
 	__repr__ = __str__
 
@@ -91,23 +108,87 @@ CURRENT_FILE = 'aoc_18_input.txt'
 start_loc = None
 keys=dict()
 doors=dict()
+loweralpha_to_num = dict()
+upperalpha_to_num = dict()
+num_to_loweralpha = dict()
+num_to_upperalpha = dict()
+num_keys = 0
+empty_key_string = ''
 
 
+def bfs(start,map):    
+	visited = []
+	queue = [start]
+ 
+	while queue:
+        
+		node = queue.pop(0)
+		if node not in visited:
+            
+			visited.append(node)
+ #           neighbours = graph[node]
+			neighbours = expand_node(node,map,keys,doors)
+			for neighbour in neighbours:
+				queue.append(neighbour)
+
+	return visited
+
+
+def expand_node(node,map,keys,doors):
+	x = node.x
+	y = node.y
+	result = list()
+
+	up = map[x][y+1]
+	down = map[x][y-1]
+	left = map[x-1][y]
+	right = map[x+1][y]
+
+	for l in [up,down,left,right]:
+		if(l=='#'):
+			continue
+		if(l=='.'):
+			result = []
+
+
+	return result
+
+def fill_alpha_to_num():
+	low_list = list(string.ascii_lowercase)
+	up_list = list(string.ascii_uppercase)
+	for i in range(0,26):
+		up_ch = up_list[i]
+		low_ch = low_list[i]
+		loweralpha_to_num[low_ch] = i
+		upperalpha_to_num[up_ch] = i
+		num_to_loweralpha[i] = low_ch
+		num_to_upperalpha[i] = up_ch
+	return 
+	
 
 def main():
 	print('AOC_2019\t 18\n\n')
 	global start_loc
 	global keys
 	global doors
-	
-	
-	
+	global loweralpha_to_num
+	global upperalpha_to_num
+	global num_to_loweralpha
+	global num_to_upperalpha
+	global num_keys
+	global empty_key_string
+
+	fill_alpha_to_num()
+
+
 	with open(CURRENT_FILE, 'r') as input_file:
 		all_file = input_file.read()
 	
 	map2 = example_1
 	#map2=all_file
 	
+	
+
 	
 	lines = map2.split('\n')
 	height = len(lines)
@@ -132,11 +213,25 @@ def main():
 
 	print_map(map)
 
+	
+
+	#find highest key
+	for y in range(0,height):
+		for x in range(0,width):
+			ch = map[x][y]
+			if(ch in string.ascii_lowercase):
+				print(ch)
+				n = loweralpha_to_num[ch]
+				num_keys = max(n,num_keys)
+	num_keys = num_keys + 1 #convert of 0-index into alphabet to number of letters
+	empty_key_string = num_keys * '-'
+	print('searching for {} keys'.format(num_keys))
+	
 	for y in range(0,height):
 		for x in range(0,width):
 			ch = map[x][y]
 			if(ch == '@'):
-				start_loc=(x,y)
+				start_loc=Location(x,y)
 				map[x][y]='.'			
 			if(ch in string.ascii_lowercase):
 				keys[(x,y)] = ch
@@ -147,10 +242,13 @@ def main():
 
 
 	print_map(map)
-	print('start: {}'.format(start_loc))
+	
+	print('start: {}'.format(start_loc))	
 	print('keys : {}'.format(keys))
 	print('doors: {}'.format(doors))
 
+	
+#	bfs(start_loc,map)
 
 
 if __name__ == "__main__":
