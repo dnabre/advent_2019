16ed57ba06a86af72f2e154d6b7ffb041b86b874
dnabre <dnabre@happybunnylabs.com>
2024-10-08 11:11:40 -0600

    Setup template for day13

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import aoc_11
+import aoc_13
 """ This module/file is just to direct execution to the current problem being worked on
 	This is only because of the (apparent) limitations of http://repl.it
 """
-aoc_11.main()
+aoc_13.main()
